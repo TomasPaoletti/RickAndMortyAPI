74f0a8e86e482d715453d5d264eeeb576288383e
Tomas Paoletti Velado <tomas.paolettiv@hotmail.com>
2023-03-16 11:57:23 +0100

    environment

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -3,9 +3,9 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: `${import.meta.env.VITE_API_KEY}`,
-    authDomain: 'pruebatecnica-a5aa8.firebaseapp.com',
-    projectId: 'pruebatecnica-a5aa8',
-    storageBucket: 'pruebatecnica-a5aa8.appspot.com',
+    authDomain: `${import.meta.env.VITE_AUTH_DOMAIN}`,
+    projectId: `${import.meta.env.VITE_PROJECT_ID}`,
+    storageBucket: `${import.meta.env.VITE_STORAGE}`,
     messagingSenderId: "1070497441755",
     appId: "1:1070497441755:web:b0e743309368f1bd12e9fe"
 };
