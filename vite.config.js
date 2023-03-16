121f2a8a8f370269c98e5b30c901338c073d00c9
Tomas Paoletti Velado <tomas.paolettiv@hotmail.com>
2023-03-16 11:49:48 +0100

    environment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  }
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      'process.env': process.env,
+    },
+    server: {
+      port: 3000,
+    }
+  })
+}
